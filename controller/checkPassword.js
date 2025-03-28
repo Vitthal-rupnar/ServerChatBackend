f228dd97cb6aa0337a46c9c7b2e23ef5171d992e
Vitthal-rupnar <vitthalrupnar8459@gmail.com>
2025-03-28 17:53:17 +0530

    first commit 5

--- controller/checkPassword.js
+++ controller/checkPassword.js
@@ -2,44 +2,62 @@ const UserModel = require("../models/UserModel")
 const bcryptjs = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
-async function checkPassword(request,response){
+async function checkPassword(request, response) {
     try {
-        const { password, userId } = request.body
+        const { password, userId } = request.body;
 
-        const user = await UserModel.findById(userId)
+        if (!password || !userId) {
+            return response.status(400).json({ 
+                message: "Password and User ID are required", 
+                error: true 
+            });
+        }
 
-        const verifyPassword = await bcryptjs.compare(password,user.password)
+        const user = await UserModel.findById(userId);
 
-        if(!verifyPassword){
+        if (!user) {
+            return response.status(404).json({ 
+                message: "User not found", 
+                error: true 
+            });
+        }
+
+        const verifyPassword = await bcryptjs.compare(password, user.password);
+
+        if (!verifyPassword) {
             return response.status(400).json({
-                message : "Please check password",
-                error : true
-            })
+                message: "Incorrect password",
+                error: true,
+            });
         }
 
         const tokenData = {
-            id : user._id,
-            email : user.email 
-        }
-        const token = await jwt.sign(tokenData,process.env.JWT_SECRET_KEY,{ expiresIn : '1d'})
+            id: user._id,
+            email: user.email,
+        };
+        const token = jwt.sign(tokenData, process.env.JWT_SECRET_KEY, { expiresIn: '10d' });
 
         const cookieOptions = {
-            http : true,
-            secure : true
-        }
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "none",
+        };
 
-        return response.cookie('token',token,cookieOptions).status(200).json({
-            message : "Login successfully",
-            token : token,
-            success :true
-        })
+        // Send token both in cookie and response
+        response.cookie('token', token, cookieOptions);
+
+        return response.status(200).json({
+            message: "Login successful",
+            token: token,
+            success: true,
+        });
 
     } catch (error) {
         return response.status(500).json({
-            message : error.message || error,
-            error : true
-        })
+            message: error.message || error,
+            error: true,
+        });
     }
 }
 
-module.exports = checkPassword
+module.exports = checkPassword;
